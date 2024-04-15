a14b25781629b00d766bfad2c4b47b060c71613a
MeteAnn <meeteann@gmail.com>
2024-04-15 17:03:08 +0300

    Cart.cs, CartLine.cs, Pages

--- StoreApp/Pages/Cart.cshtml.cs
+++ StoreApp/Pages/Cart.cshtml.cs
@@ -1,13 +1,41 @@
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Contracts;
 
 namespace StoreApp.Pages
 {
     
     public class CartModel : PageModel
     {
+            private readonly IServiceManager _manager;
+            public Cart Cart {get; set;} //IoC
+            public string ReturnUrl {get; set;} = "/";
+
+            public void OnGet(string returnUrl)
+            {
 
 
+                ReturnUrl = returnUrl ?? "/";
 
+
+            }
+
+            public IActionResult OnPost(int productId, string returnUrl)
+            {
+
+
+                Product? product = _manager.ProductService.GetOneProduct(productId, false);
+            
+                if (product is not null)
+                {
+
+                    Cart.AddItem(product,1);
+                    
+                }
+
+                return Page();//returnUrl
+            }
         
     }
 
