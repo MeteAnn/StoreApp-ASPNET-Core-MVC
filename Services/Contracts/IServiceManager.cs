cc81801a130f63735dfb9fafc66dd170285f870d
MeteAnn <meeteann@gmail.com>
2024-04-22 17:18:09 +0300

    Identy

--- Services/Contracts/IServiceManager.cs
+++ Services/Contracts/IServiceManager.cs
@@ -1,14 +1,10 @@
-using Entities.Models;
-
 namespace Services.Contracts
 {
     public interface IServiceManager
     {
         IProductService ProductService {get;}
         ICategoryService CategoryService {get;}
-
         IOrderService OrderService {get;}
-
-
+        IAuthService AuthService {get; }
     }
 }
--- Services/ServiceManager.cs
+++ Services/ServiceManager.cs
@@ -7,17 +7,17 @@ namespace Services
         private readonly IProductService _productService;
         private readonly ICategoryService _categoryService;
         private readonly IOrderService _orderService;
-        
+        private readonly IAuthService _authService;
 
         public ServiceManager(IProductService productService,
         ICategoryService categoryService,
-        IOrderService orderService
-        )
+        IOrderService orderService,
+        IAuthService authService)
         {
             _productService = productService;
             _categoryService = categoryService;
             _orderService = orderService;
-          
+            _authService = authService;
         }
 
         public IProductService ProductService => _productService;
@@ -26,6 +26,6 @@ namespace Services
 
         public IOrderService OrderService => _orderService;
 
-        
+        public IAuthService AuthService => _authService;
     }
 }
