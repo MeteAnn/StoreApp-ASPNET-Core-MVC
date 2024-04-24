9d4bb9f0a374f8a97a24eee99cd54e0a86f42b3d
MeteAnn <meeteann@gmail.com>
2024-04-24 11:23:02 +0300

    API

--- Entities/Models/Product.cs
+++ Entities/Models/Product.cs
@@ -1,4 +1,4 @@
-﻿﻿using System.ComponentModel.DataAnnotations;
+﻿﻿﻿using System.ComponentModel.DataAnnotations;
 
 namespace Entities.Models;
 public class Product
---
+++ Presentation/AssemblyReference.cs
@@ -0,0 +1,7 @@
+﻿﻿namespace Presentation
+{
+    public class AssemblyReference
+    {
+
+    }
+}
