8b59ee3a82dd3b44c44f3700d76f853af946e56b
MeteAnn <meeteann@gmail.com>
2024-04-24 11:38:49 +0300

    AdminPanel

--- StoreApp/Components/ProductSummaryViewComponent.cs
+++ StoreApp/Components/ProductSummaryViewComponent.cs
@@ -4,31 +4,19 @@ using Services.Contracts;
 
 namespace StoreApp.Components
 {
-    
     public class ProductSummaryViewComponent : ViewComponent
-    {   
-
-        private readonly IServiceManager _manager;  // 
+    {
+        private readonly IServiceManager _manager;
 
         public ProductSummaryViewComponent(IServiceManager manager)
         {
-
-
             _manager = manager;
-
-
         }
 
         public string Invoke()
         {
-        
+            // service
             return _manager.ProductService.GetAllProducts(false).Count().ToString();
-
-            
-
         }
-
-
     }
-
 }
